d92837b2496897144b39edcf7a31849ef3fd471b
Joseph E Auz <joseph.auz77@gmail.com>
2020-03-27 05:37:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ end
 def summon_captain_planet(source)
   new_array = [] 
   source.collect do |ele|
-    new_array
+    new_array << "#{ele.capitalize}!"
   end 
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
