8feff0d667b9426c24e0cfc316c8db82700f5682
Joseph E Auz <joseph.auz77@gmail.com>
2020-03-27 05:37:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(source)
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(source)
+  new_array = [] 
+  source.collect do |ele|
+    new_array
+  end 
 end
 
 def long_planeteer_calls# code an argument here
