2e59b0585d62cfa1f24e2896825191e772040100
Joseph E Auz <joseph.auz77@gmail.com>
2020-03-27 05:41:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(source)
   source.any? {|words| words.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(source)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  source.find {|cheese| cheese == cheese_types
 end
